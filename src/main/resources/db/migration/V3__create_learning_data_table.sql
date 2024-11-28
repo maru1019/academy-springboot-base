4280c4ff746302b43796403c170e52fc9f0d0b25
maru1019 <green101927.10@gmail.com>
2024-11-29 00:04:05 +0900

    マイグレーションsql追加

---
+++ src/main/resources/db/migration/V3__create_learning_data_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE learning_data (
+  id SERIAL PRIMARY KEY,
+  create_month INT NOT NULL,
+  name VARCHAR(10) NOT NULL,
+  study_time INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted_at TIMESTAMP NOT NULL,
+  user_id INT NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
