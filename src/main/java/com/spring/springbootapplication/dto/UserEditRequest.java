f8a152d6d1e9068f7f1c9b66a1014c4f0d8153b7
maru1019 <green101927.10@gmail.com>
2024-12-27 02:06:58 +0900

    バリデーション修正

--- src/main/java/com/spring/springbootapplication/dto/UserEditRequest.java
+++ src/main/java/com/spring/springbootapplication/dto/UserEditRequest.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -13,7 +13,7 @@ public class UserEditRequest implements Serializable {
 
   private Integer id;
 
-  @NotBlank(message = "")
+  @NotEmpty
   @Size(min = 50, max = 200, message = "自己紹介は50文字以上200文字以下で入力してください")
   private String biography;
 
