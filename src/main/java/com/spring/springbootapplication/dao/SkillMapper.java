1c9b4b4cfacce0d9bac48ad734901b82a559ac5b
maru1019 <green101927.10@gmail.com>
2025-01-08 00:38:12 +0900

    項目一覧ページ要件実装

--- src/main/java/com/spring/springbootapplication/dao/SkillMapper.java
+++ src/main/java/com/spring/springbootapplication/dao/SkillMapper.java
@@ -3,12 +3,18 @@ package com.spring.springbootapplication.dao;
 import org.apache.ibatis.annotations.Mapper;
 import com.spring.springbootapplication.dto.SkillRequest;
 import com.spring.springbootapplication.entity.SkillEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
 
 @Mapper
 public interface SkillMapper {
 
-    SkillEntity findById(Integer id);
+    // 指定した月とユーザーIDでデータを検索
+    List<SkillEntity> findByMonthAndUser(@Param("month") Integer month, @Param("userId") Integer userId);
 
-    void save(SkillRequest skillRequest);
+    // void save(SkillRequest skillRequest);
     
 }
