4b768c11d7569d5f801a80f238496972ce4ebb91
maru1019 <green101927.10@gmail.com>
2025-02-03 01:31:32 +0900

    フロントエンド修正

--- src/main/java/com/spring/springbootapplication/dto/SkillRequest.java
+++ src/main/java/com/spring/springbootapplication/dto/SkillRequest.java
@@ -24,7 +24,7 @@ public class SkillRequest implements Serializable {
 
   @NotNull(message = "学習時間は必ず入力してください")
   @Min(value = 0, message = "学習時間は0以上の数字で入力してください")
-  private Integer studyTime = 0;
+  private Integer studyTime;
 
   private Integer userId;
 
